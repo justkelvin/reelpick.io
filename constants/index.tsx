74cc108b56055f0a93a9d77f19d423e2855d0c5c
justkelvin <muia.muinde@s.karu.ac.ke>
2024-06-03 11:27:22 +0300

    add route constants

---
+++ constants/index.tsx
@@ -0,0 +1,27 @@
+export const SidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg',
+    },
+    {
+        label: 'Movies',
+        route: '/movies',
+        imgUrl: '/icons/movie.svg',
+    },
+    {
+        label: 'Shows',
+        route: '/shows',
+        imgUrl: '/icons/show.svg',
+    },
+    {
+        label: 'Library',
+        route: '/library',
+        imgUrl: '/icons/library.svg',
+    },
+        {
+        label: 'Profile',
+        route: '/profile',
+        imgUrl: '/icons/profile.svg',
+    }
+]
